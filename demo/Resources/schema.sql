7ceb5c1bf5c643cdbf797d8b91ae025eb37a0ed7
Olaleke Sangogade <105587442+olalekeolaleke@users.noreply.github.com>
2022-09-22 21:32:10 +0100

    Adding changes

--- demo/Resources/schema.sql
+++ demo/Resources/schema.sql
@@ -2,33 +2,27 @@
 
  DROP TABLE IF EXISTS public.world_population;
 
-CREATE TABLE IF NOT EXISTS public.world_population
-(
-    country_name text primary key,
-    "2015" bigint,
-    "2016" bigint,
-    "2017" bigint,
-    "2018" bigint,
-    "2019" bigint,
-    "2020" bigint,
-    "2021" bigint    
-)
+CREATE TABLE public.world_population(
+    id serial primary key,
+    country_name text,
+    variable text,
+    value bigint
+);
+
 
 -- Table: public.unemployment_rate
 
-DROP TABLE IF EXISTS public.unemployment_rate;
+ DROP TABLE IF EXISTS public.unemployment_rate;
 
-CREATE TABLE IF NOT EXISTS public.unemployment_rate
-(
-    country_name text primary key,
-    "2015" int,
-    "2016" int,
-    "2017" int,
-    "2018" int,
-    "2019" int,
-    "2020" int,
-    "2021" int    
-)
+CREATE TABLE IF NOT EXISTS public.unemployment_rate(
+    
+    id serial primary key,
+    country_name text,
+    variable text,
+    value double precision
+);
+
+    
 -- Displaying everything from wolrd_population table 
 
 select * from public.world_population;
