91406cc951fb3886e9d0bb47b12e6c0db861157e
Olaleke Sangogade <105587442+olalekeolaleke@users.noreply.github.com>
2022-09-22 15:55:47 +0100

    Updating folders and files

--- demo/Resources/schema.sql
+++ demo/Resources/schema.sql
@@ -1,22 +1,31 @@
+-- Table: public.world_population
+
+-- DROP TABLE IF EXISTS public.world_population;
+
+CREATE TABLE IF NOT EXISTS public.world_population
+(
+    country_name text COLLATE pg_catalog."default",
+    variable text COLLATE pg_catalog."default",
+    value bigint
+)
+
+TABLESPACE pg_default;
+
+
+
+ALTER TABLE IF EXISTS public.world_population
+    OWNER to postgres;
+    
+
 -- Table: public.unemployment_rate
 
 -- DROP TABLE IF EXISTS public.unemployment_rate;
 
 CREATE TABLE IF NOT EXISTS public.unemployment_rate
 (
-    country_name text COLLATE pg_catalog."default" NOT NULL,
-    "2011" integer,
-    "2012" integer,
-    "2013" integer,
-    "2014" integer,
-    "2015" integer,
-    "2016" integer,
-    "2017" integer,
-    "2018" integer,
-    "2019" integer,
-    "2020" integer,
-    "2021" integer,
-    CONSTRAINT unemployment_rate_pkey PRIMARY KEY (country_name)
+    country_name text COLLATE pg_catalog."default",
+    variable text COLLATE pg_catalog."default",
+    value double precision
 )
 
 TABLESPACE pg_default;
@@ -24,32 +33,10 @@ TABLESPACE pg_default;
 ALTER TABLE IF EXISTS public.unemployment_rate
     OWNER to postgres;
     
--- Table: public.world_population
+-- Displaying everything from wolrd_population table 
 
--- DROP TABLE IF EXISTS public.world_population;
+select * from public.world_population;
 
-CREATE TABLE IF NOT EXISTS public.world_population
-(
-    country_name text COLLATE pg_catalog."default" NOT NULL,
-    "2011" bigint,
-    "2012" bigint,
-    "2013" bigint,
-    "2014" bigint,
-    "2015" bigint,
-    "2016" bigint,
-    "2017" bigint,
-    "2018" bigint,
-    "2019" bigint,
-    "2020" bigint,
-    "2021" bigint,
-    CONSTRAINT world_population_pkey PRIMARY KEY (country_name)
-)
+-- Displaying everything from unemployment_rate table 
 
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS public.world_population
-    OWNER to postgres;
-    
-select * from unemployment_rate;
-
-select * from world_population;
+select * from public.unemployment_rate;
