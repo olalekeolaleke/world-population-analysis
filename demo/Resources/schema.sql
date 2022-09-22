a53ea2d51516156b5f0d2bacaed9125ad0667508
Olaleke Sangogade <105587442+olalekeolaleke@users.noreply.github.com>
2022-09-22 20:49:41 +0100

    Adding changes to Resorces folder

--- demo/Resources/schema.sql
+++ demo/Resources/schema.sql
@@ -1,38 +1,34 @@
 -- Table: public.world_population
 
--- DROP TABLE IF EXISTS public.world_population;
+ DROP TABLE IF EXISTS public.world_population;
 
 CREATE TABLE IF NOT EXISTS public.world_population
 (
-    country_name text COLLATE pg_catalog."default",
-    variable text COLLATE pg_catalog."default",
-    value bigint
+    country_name text primary key,
+    "2015" bigint,
+    "2016" bigint,
+    "2017" bigint,
+    "2018" bigint,
+    "2019" bigint,
+    "2020" bigint,
+    "2021" bigint    
 )
-
-TABLESPACE pg_default;
-
-
-
-ALTER TABLE IF EXISTS public.world_population
-    OWNER to postgres;
-    
 
 -- Table: public.unemployment_rate
 
--- DROP TABLE IF EXISTS public.unemployment_rate;
+DROP TABLE IF EXISTS public.unemployment_rate;
 
 CREATE TABLE IF NOT EXISTS public.unemployment_rate
 (
-    country_name text COLLATE pg_catalog."default",
-    variable text COLLATE pg_catalog."default",
-    value double precision
+    country_name text primary key,
+    "2015" int,
+    "2016" int,
+    "2017" int,
+    "2018" int,
+    "2019" int,
+    "2020" int,
+    "2021" int    
 )
-
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS public.unemployment_rate
-    OWNER to postgres;
-    
 -- Displaying everything from wolrd_population table 
 
 select * from public.world_population;
